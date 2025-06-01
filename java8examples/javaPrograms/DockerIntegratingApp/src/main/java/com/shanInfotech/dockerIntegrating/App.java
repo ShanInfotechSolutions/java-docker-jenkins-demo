4e7e2b6ba66d2692ac771a30b9ea3fe16927d4e9
ShanInfotechSolutions <shanmukh@shaninfotechsolutions.com>
2025-06-02 01:53:02 +0530

    App Class Modified

--- java8examples/javaPrograms/DockerIntegratingApp/src/main/java/com/shanInfotech/dockerIntegrating/App.java
+++ java8examples/javaPrograms/DockerIntegratingApp/src/main/java/com/shanInfotech/dockerIntegrating/App.java
@@ -2,6 +2,6 @@ package com.shanInfotech.dockerIntegrating;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Dockerized Maven App!");
+        System.out.println("Hello Ganesha! Hello from Dockerized Maven App!");
     }
 }
